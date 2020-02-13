37e9f4f3e6f9f27ae62877cd34571db1ec6f93ca
J Quinn <git@jaquinn.com>
2020-02-13 06:36:28 +0100

    Added alerts

--- State/src/components/HelloWorld/index.tsx
+++ State/src/components/HelloWorld/index.tsx
@@ -1,23 +1,43 @@
 import React, { FC, useContext, useEffect, useState } from "react";
 
-import { AddAlert } from "context/alert/IAlert";
+import { AddAlert, ClearAlerts } from "context/alert/IAlert";
 
 import AlertContext from "context/alert/AlertContext";
 
 const HelloWorld: FC = () => {
   const alertContext = useContext(AlertContext);
-  const { addAlert }: { addAlert: AddAlert } = alertContext;
+  const {
+    addAlert,
+    clearAlerts
+  }: { addAlert: AddAlert; clearAlerts: ClearAlerts } = alertContext;
 
   const [loading, setLoading] = useState(true);
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     if (!loading) return;
     setLoading(false);
 
-    addAlert("Hi", "danger");
-  }, [addAlert]);
+    addAlert("Hi", "danger", 5000);
+  }, [loading, addAlert]);
 
-  return <h1>Hello World</h1>;
+  useEffect(() => {
+    addAlert(`Hello: ${counter}`, "light");
+  }, [counter]);
+
+  return (
+    <div className="container flex col center">
+      <h1>Hello World</h1>
+      <div className="row">
+        <button className="btn m" onClick={() => setCounter(counter + 1)}>
+          Button
+        </button>
+        <button className="btn m" onClick={clearAlerts}>
+          Clear Alerts
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default HelloWorld;
