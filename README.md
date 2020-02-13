956595fed8300f0588043329efb2c5a30b2180aa
J Quinn <git@jaquinn.com>
2020-02-13 04:17:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Template
+
+Clean starting point using TypeScript, Yarn, Webpack, Babel and ESLint.
+
+Typechecking is being done with the ts fork plugin.
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import HelloWorld from "components/HelloWorld";
+
+const App: FC = () => (
+  <>
+    <HelloWorld />
+  </>
+);
+
+export default App;
